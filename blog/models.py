cfa438fc4bf18e28c85a574964d34b7c2ada0737
rajatjasrotia <rajatjasrotia150395@gmail.com>
2021-09-13 23:41:18 +0530

    Added blog functionality

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,9 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+    def summary(self):
+        return self.body[:100]
+
+    def __str__(self):
+        return self.title
